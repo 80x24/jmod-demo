10ed7fba78a8a56091745697436db9d861aa78cb
Kyle Schreiber <kyle@80x24.net>
2019-01-08 19:40:51 -0800

    Created mods folder

--- compile.sh
+++ compile.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # compile
-javac --module-path lib -d lib/crypto src/module-info.java src/crypto/Main.java
+javac --module-path lib -d mods/crypto src/module-info.java src/crypto/Main.java
 
 # create jar
-jar -c --file=lib/crypto.jar --main-class crypto.Main -C lib/crypto .
-rm -rf lib/crypto
+jar -c --file=lib/crypto.jar --main-class crypto.Main -C mods/crypto .
--- make-runtime.sh
+++ make-runtime.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules java.base,java.naming,java.sql --output java-runtime
+jlink --no-header-files --no-man-pages --compress 2 --strip-debug --add-modules java.base,java.naming,java.sql --output java-runtime
